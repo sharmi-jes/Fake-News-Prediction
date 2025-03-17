35aa8b0a749232f5278ab79d331ac4a6e993eac7
sharmi-jes <anyumsharmila@gmail.com>
2025-03-17 15:21:38 +0530

    added app.py

--- app.py
+++ app.py
@@ -6,9 +6,36 @@ from sklearn.metrics import accuracy_score
 
 app=Flask(__name__)
 
+
+# load the all modesl
+import joblib
+
+tfidf = joblib.load('tfidf_vectorizer.pkl')
+
+
+pca = joblib.load('pca_transformer.pkl')
+
+
+normalizer = joblib.load('normalizer.pkl')
+
+
+model = joblib.load('news_classifier_model.pkl')
+
+print("All components loaded successfully!")
+
+
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/predict",methods=["GET","POST"])
+def predictdata():
+    if request.method=='GET':
+        return render_template('home.html')
+    
+    else:
+        
 
 
 if __name__=="__main__":
